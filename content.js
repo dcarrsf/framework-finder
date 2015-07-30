be041358ee6af53307e6ef1c9a97a7f5ad79b349
Andrew Carlson <acarl005@g.ucla.edu>
2015-07-30 14:03:09 -0700

    fix page load issue!!!!!

--- content.js
+++ content.js
@@ -1,12 +1,14 @@
 var angular = /ng-app|ng-controller|ng-click|ng-scope|ng-if|ng-show|ng-hide|ng-submit|ng-model|ng-repeat|ng-class|ng-bind|ng-href|ui-view/;
 var react = /data-reactid/;
 
-window.addEventListener('load', function() {
+scanDOM();
+window.addEventListener('load', scanDOM, false);
 
-  if (document.body.innerHTML.match(angular)) {
+function scanDOM() {
+  var page = document.body.innerHTML;
+  if (page.match(angular)) {
     chrome.runtime.sendMessage({ found: 'angular' });
-  } else if (document.body.innerHTML.match(react)) {
+  } else if (page.match(react)) {
     chrome.runtime.sendMessage({ found: 'react' });
   }
-
-}, false);
+}
