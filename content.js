03f290c9dea100a6b604b71f1b0469e3b7912e08
Andrew Carlson <acarl005@g.ucla.edu>
2015-07-30 15:12:51 -0700

    acutally searches attributes instead of plain text

--- content.js
+++ content.js
@@ -1,14 +1,38 @@
-var angular = /ng-app|ng-controller|ng-click|ng-scope|ng-if|ng-show|ng-hide|ng-submit|ng-model|ng-repeat|ng-class|ng-bind|ng-href|ui-view/;
-var react = /data-reactid/;
+// var angular = /ng-app|ng-controller|ng-click|ng-scope|ng-if|ng-show|ng-hide|ng-submit|ng-model|ng-repeat|ng-class|ng-bind|ng-href|ui-view/;
+// var react = /data-reactid/;
+
+var angularAttrs = [
+  '[ng-app]',
+  '[ng-controller]',
+  '[ng-click]',
+  '[ng-scope]',
+  '[ng-if]',
+  '[ng-show]',
+  '[ng-hide]',
+  '[ng-submit]',
+  '[ng-model]',
+  '[ng-repeat]',
+  '[ng-class]',
+  '[ng-bind]',
+  '[ng-href]',
+  '[ng-view]',
+  '[ui-view]'
+]
 
 scanDOM();
 window.addEventListener('load', scanDOM, false);
 
 function scanDOM() {
   var page = document.body.innerHTML;
-  if (page.match(angular)) {
+  if (angular()) {
     chrome.runtime.sendMessage({ found: 'angular' });
-  } else if (page.match(react)) {
+  } else if (!!document.querySelector('[data-reactid]')) {
     chrome.runtime.sendMessage({ found: 'react' });
   }
+}
+
+function angular() {
+  return angularAttrs.reduce(function(bool, attr) {
+    return bool || !!document.querySelector(attr);
+  }, false);
 }
